e0f47ff4709c39d8686be5de09fb135d7980f67e
schegu <chskeerthana@gmail.com>
2016-05-27 00:13:53 -0700

    Add files via upload

---
+++ syamalakeerthana-278-5pm-assignment4/main.rb
@@ -0,0 +1,61 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'erb'
+require './student'
+
+configure do   #Enabling session and required config settings
+  enable :sessions
+  set :username, 'keerthana'
+  set :password, 'shroff'
+  set :admin, 'false'
+end
+
+configure :development do  #Database set up
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/studentinfo.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
+#Login implementation
+get '/login' do
+  erb :login
+ end
+
+post '/login' do
+ if params[:username] == settings.username &&  params[:password] == settings.password 
+             session[:admin] = true
+             redirect to ('/students')
+     else   
+      erb :login   
+      'Enter correct credentials'
+
+ end
+end
+
+#Logout implementation
+get '/logout' do
+   session[:admin] = false
+
+   redirect to ('/') 
+end
+
+#Home page route
+get '/' do
+ erb :home
+end
+
+#about page route
+get '/about' do
+ erb :about
+end
+
+#contact page route
+get '/contact' do
+ erb :contact
+end
+
+#linking styles
+get('/styles.css'){ scss :styles }
+
